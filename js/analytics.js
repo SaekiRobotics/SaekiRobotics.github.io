147d40ad1a785119d91b3e6a58fc946b76c9102c
Oliver Harley <oliver.r.harley@gmail.com>
2021-02-22 13:54:59 +0100

    google analytics

---
+++ js/analytics.js
@@ -0,0 +1,29 @@
+
+<!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "ce98840acd1a404e8b96a95c156bfd1b"}'></script><!-- End Cloudflare Web Analytics -->
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7CFYYFHKBG"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-7CFYYFHKBG');
+</script>
+
+// <!-- Google Tag Manager -->
+//     // Set up loading of ad JavaScript. For example, GPT might look something like:
+//     <script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
+//     <script>
+//     googletag.cmd.push(function() {
+//         googletag.defineSlot(...);
+//         googletag.pubads().enableSingleRequest();
+//         googletag.enableServices();
+//     });
+// </script>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MHTPZGD');</script>
+<!-- End Google Tag Manager -->
